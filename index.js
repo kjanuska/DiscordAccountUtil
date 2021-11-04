fdcb09dc2aff9d622754e07af252efae70a33427
Kipras Januska <kiprasjanuska0@gmail.com>
2021-11-04 14:10:57 -0400

    Fix loop timer and make loop wait random time between 60 and 15 seconds

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const axios = require('axios').default;
 const readline = require("readline");
 const fs = require('fs');
-const wait = require('util').promisify(setTimeout);
 
 const userInput = readline.createInterface({
     input: process.stdin,
@@ -13,14 +12,21 @@ async function join()
     let text = fs.readFileSync("./tokens.txt", "utf-8");
     const TOKENS = text.split("\n")
     userInput.question("Server invite code: ", function(code) {
-        TOKENS.forEach(async (token) => {
-            await axios.post(`https://discordapp.com/api/v9/invites/${code}`,
-                {
-                    headers: { 'authorization': token }
-                });
-            await wait(5000);
+        TOKENS.forEach(async (token, index) => {
+            let waitTime = Math.floor(Math.random() * (60000 - 15000) + 15000)
+            setTimeout(async () => {
+                try {
+                    await axios.post(`https://discordapp.com/api/v9/invites/${code}`,
+                    {
+                        headers: { 'authorization': token }
+                    });
+                } catch (error) {
+                    console.log(error);
+                }
+            }, waitTime * index);
         })
         userInput.close();
     });}
+    console.log("All accounts joined!");
 
 join();
