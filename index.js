f48c56507d5b8fa20e2d5a3e3703cdc66cfe295e
Kipras <kiprasjanuska0@gmail.com>
2021-11-08 12:26:27 -0500

    Change to python because javascript is weird

--- index.js
+++ index.js
@@ -12,13 +12,17 @@ async function join()
     let text = fs.readFileSync("./tokens.txt", "utf-8");
     const TOKENS = text.split("\n")
     userInput.question("Server invite code: ", function(code) {
+        code = code.replace('https://discord.gg/', '');
         TOKENS.forEach(async (token, index) => {
             let waitTime = Math.floor(Math.random() * (60000 - 15000) + 15000)
             setTimeout(async () => {
                 try {
                     await axios.post(`https://discordapp.com/api/v9/invites/${code}`,
                     {
-                        headers: { 'authorization': token }
+                        headers: {
+                            'authorization': token,
+                            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
+                        }
                     });
                 } catch (error) {
                     console.log(error);
@@ -27,7 +31,5 @@ async function join()
         })
         userInput.close();
     });
-    console.log("All accounts joined!");
+    // await console.log("All accounts joined!");
 }
-
-join();
---
+++ join.py
@@ -0,0 +1,7 @@
+import requests
+
+headers = {
+    'authorization': 'ODk2NDI1MzcwNTI4NjY1NjAw.YYlJvw.4JJeWyXOn7ijRrGW0Z0vQhSnJKc'
+}
+
+requests.post('https://discordapp.com/api/v9/invites/aaNCY6jW', headers=headers)
