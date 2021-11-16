e766a2596ef677968149c7c1f0faa4908fd45835
Kipras Januska <kiprasjanuska0@gmail.com>
2021-11-16 14:08:59 -0500

    Begin methods for creating accounts + verifying email & phone

---
+++ text.py
@@ -0,0 +1,7 @@
+import requests
+
+def get_balance():
+    return
+
+def get_code():
+    return
--- util.py
+++ util.py
@@ -6,6 +6,8 @@ from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
 
+from requests.api import head
+
 load_dotenv()
 
 ENTRY = "https://discordapp.com/api/v9"
@@ -122,8 +124,70 @@ def leave_all_servers():
             time.sleep(2)
         time.sleep(10)
 
+def get_captcha_key():
+    CAP_KEY = os.environ["2CAP_API_KEY"]
+    SITE_KEY = "f5561ba9-8f1e-40ca-9b5b-a0b3f719ef34"
+    resp = requests.post(f"http://2captcha.com/in.php?key={CAP_KEY}&method=hcaptcha&sitekey={SITE_KEY}&pageurl=https://discord.com/register&json=1").json()
+    if resp["status"] != 1:
+        print("Error sending catpcha to provider:\n" + resp)
+        return
+    captcha_id = resp["request"]
+    time.sleep(20)
+    resp = requests.get(f"http://2captcha.com/res.php?key={CAP_KEY}&action=get&id={captcha_id}&json=1").json()
+    print(resp)
+
+def verify_phone(token):
+    # verify phone number
+    PHONE_ENDPOINT = "/users/@me/phone"
+    header = {
+        "authorization" : token,
+        "content-type": "application/json",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36"
+    }
+    data = {
+        "phone": phone
+    }
+
+    # send code to phone number
+    resp = requests.post(f"{ENTRY}{PHONE_ENDPOINT}", headers=header, json=data)
+
+    # get code from Text Verified
+
+    # send verification code
+    CODE_ENDPOINT = "/phone-verifications/verify"
+    data["code"] = code
+
+    resp = requests.post(f"{ENTRY}{CODE_ENDPOINT}", headers=header, json=data)
+    if resp.status_code == 400:
+        print("Verification code incorrect")
+
 def create_account():
+    ENDPOINT = "/auth/register"
+    header = {
+        "content-type": "application/json",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36"
+    }
+    captcha_key = get_captcha_key()
+
+    data = {
+        "captcha_key": captcha_key,
+        "consent": True,
+        "date_of_birth": "1976-04-18",
+        "email": email,
+        "fingerprint": fingerprint,
+        "password": password,
+        "username": username
+    }
+
+    resp = requests.post(f"{ENTRY}{ENDPOINT}", headers=header, json=data).json()
+    token = resp["token"]
     token_doc = {
         "token" : encryptor.encrypt(token.encode())
     }
     tokens.insert_one(token_doc)
+
+    # verify through email
+    # create account using catchall and then access verification link from main gmail
+    # use gmail API to fetch emails and post a request to the verification URL
+
+    verify_phone(token)
